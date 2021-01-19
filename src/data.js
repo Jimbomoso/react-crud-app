1b9aea48e2a0f2cc60781b83d26dbb7cac2b1e0f
J.C <jimbomoso@gmail.com>
2021-01-19 15:31:33 -0500

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,16 @@
+const userList = [
+    {
+        id: 1,
+        name: 'Bob',
+        username: 'Bob',
+    },
+
+    {
+        id: 2,
+        name: 'Bird',
+        username: 'Big Bird',
+
+    },
+];
+
+export default userList;
